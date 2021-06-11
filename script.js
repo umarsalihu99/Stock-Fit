d1b934205c627fb404221a1f42fa5a1650ff2fd0
Umar <81313320+umarsalihu99@users.noreply.github.com>
2021-06-11 13:19:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var smedia_icons = document.getElementById("media_icons");
+        function showMenu() {
+            media_icons.style.right = "0";
+        }
+        function hideMenu() {
+            media_icons.style.right = "-500px";
+        }
